d43a93703d8e5cb5b4597a8124a7cd713565fb51
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-05-26 22:01:54 -0600

    Cool, found a clever way to ensure the right system is set, and also compute checksum (not yet implemented)

--- protobufs/src/subsystems/imu.cpp
+++ protobufs/src/subsystems/imu.cpp
@@ -5,10 +5,8 @@ namespace subsystems {
 namespace imu {
 bool collect_telemetry(cdh::telemetry::TMFrame &frame) {
   cdh::telemetry::Telemetry *tm0 = frame.add_tm();
-  tm0->set_sys(IMU); // XXX: This is error prone because the human decides the
-                     // subsytem it's applicable to. Not sure how to make this
-                     // much better from the PB def.
   tm0->set_id(0x01);
+  tm_chk(tm0);
   return true;
 };
 }
--- protobufs/src/subsystems/imu.hpp
+++ protobufs/src/subsystems/imu.hpp
@@ -7,6 +7,10 @@ namespace cdh {
 namespace subsystems {
 namespace imu {
 bool collect_telemetry(cdh::telemetry::TMFrame &frame);
+inline void tm_chk(cdh::telemetry::Telemetry *tm) {
+  tm->set_sys(IMU);
+  tm->set_checksum(0);
+};
 }
 }
 };
