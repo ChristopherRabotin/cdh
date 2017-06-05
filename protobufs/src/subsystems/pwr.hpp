35765390afba8e3ee975fe30fad1e513ec3ae078
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-06-05 00:38:41 -0600

    Done with subsystem tests and basic code
    Up now, in the next issue, is generating all this. After that, I'll need to work on the ZephyrRTOS integration.

--- protobufs/src/subsystems/pwr.hpp
+++ protobufs/src/subsystems/pwr.hpp
@@ -2,11 +2,17 @@
 #define PWR_HPP_INCLUDED
 
 #include "../proto/telemetry.pb.h"
+#include "../utils.hpp"
 
 namespace cdh {
 namespace subsystems {
 namespace pwr {
 bool collect_telemetry(cdh::telemetry::TMFrame &frame);
+inline void tm_chk(cdh::telemetry::Telemetry *tm) {
+  tm->set_sys(PWR);
+  tm->set_checksum(compute_crc32(tm->SerializeAsString()));
+  tm->set_timestamp(millis_since_epoch());
+};
 }
 }
 };
