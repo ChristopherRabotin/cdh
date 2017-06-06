aa58432b927b240fc0c17d8d1e2e10a88f7b29d7
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-06-06 11:14:16 -0600

    Minor tc test, will add a CDH_ERR TM

--- protobufs/src/tests/tctest.cpp
+++ protobufs/src/tests/tctest.cpp
@@ -3,114 +3,33 @@
 
 #include "../cdh.hpp"
 #include "../proto/telemetry.pb.h"
+#include "../utils.hpp"
 #include <map>
 #include <stdint.h>
 
-SCENARIO("Check that subsystems are fully defined", "[bdd][cdh][subsystems]") {
-  GIVEN("The subsystem enum") {
-    using namespace cdh::subsystems;
-    WHEN("iterating over the enum") {
-      THEN("the symmetry in numbering should be correct") {
-        int subsys_id = CDH;
-        do {
-          INFO("checking subsys_id = " << subsys_id << " out of " << HMI);
-          REQUIRE(subsystem_from_ID(subsys_id) == subsys_id);
-          AND_THEN(
-              "it should also have a std::string name and can be reversed");
-          REQUIRE(subsystem_from_name(subsystem_name(
-                      subsystem_from_ID(subsys_id))) == subsys_id);
-          subsys_id++;
-        } while (subsys_id < HMI);
-      }
-    }
-  }
-}
+/*
+IDEA: check the order of execution by having multiple "increment/decrement" and
+"toggle" calls before reading the telemetry. Also ensure that single frame or no
+frame works.
+*/
 
-SCENARIO("Mock telemetry", "[bdd][cdh][telemetry]") {
-  GIVEN("Some mock telemetry collection,") {
-    WHEN("Collecting all telemetry,") {
-      cdh::telemetry::TMFrame frame = cdh::subsystems::collect_all_telemetry();
-      THEN("the first seq_no should be 0,") {
-        REQUIRE(frame.sequence_no() == 0);
+SCENARIO("Mock telecommand", "[bdd][cdh][telecommand]") {
+  GIVEN("Some mock telecommand logic,") {
+    WHEN("A TCFrame is valid,") {
+      cdh::telecommand::TCFrame frame;
+      THEN("if it is in time stamped in the future") {
+        frame.set_timestamp(millis_since_epoch() + 1000);
+        frame.set_sequence_no(0);
+        cdh::subsystems::process_all_telecommands(frame);
+        INFO("should print a time warning");
+        REQUIRE(true);
       }
-      THEN("the second seq_no should be 1,") {
-        REQUIRE(frame.sequence_no() == 1);
-      }
-      THEN("the first management telemetry point should be a valid CDH frame") {
-        cdh::telemetry::Telemetry cdh_mgmt = frame.tm(0);
-        REQUIRE(cdh_mgmt.sys() == cdh::subsystems::CDH);
-        AND_THEN("its ID should 0x01");
-        REQUIRE(cdh_mgmt.id() == 0x01);
-        AND_THEN("the case of the data should be int");
-        REQUIRE(cdh_mgmt.data_case() ==
-                cdh::telemetry::Telemetry::DataCase::kIntValue);
-        AND_THEN("the max sequence number should be set to the limit of uint");
-        // NOTE: We convert this manually to a unit because the TM does not
-        // support unit, only int, but we know what this should be.
-        REQUIRE((uint)cdh_mgmt.int_value() == UINT_MAX);
-      }
-      THEN("each subsystem should have the same mock telemetry points") {
-        // NOTE: Using `.at()` for the maps to throw an exception if unknown
-        // key.
-        using namespace cdh::subsystems;
-        std::map<Subsystem, char> tm_count;
-        tm_count[PWR] = 0;
-        tm_count[IMU] = 0;
-        tm_count[HMI] = 0;
-        std::map<Subsystem, char> tm_id;
-        tm_id[PWR] = 0;
-        tm_id[IMU] = 0;
-        tm_id[HMI] = 0;
-        std::map<int, cdh::telemetry::Telemetry::DataCase> tm_data_case;
-        tm_data_case[0] = cdh::telemetry::Telemetry::DataCase::kBoolValue;
-        tm_data_case[10] = cdh::telemetry::Telemetry::DataCase::kDoubleValue;
-        tm_data_case[20] = cdh::telemetry::Telemetry::DataCase::kIntValue;
-        tm_data_case[30] = cdh::telemetry::Telemetry::DataCase::kBytesValue;
-        tm_data_case[40] = cdh::telemetry::Telemetry::DataCase::DATA_NOT_SET;
-        tm_data_case[50] = cdh::telemetry::Telemetry::DataCase::kBoolValue;
-        for (int tm_frame = 1; tm_frame < frame.tm_size(); tm_frame++) {
-          cdh::telemetry::Telemetry this_tm = frame.tm(tm_frame);
-          Subsystem subsys = this_tm.sys();
-          switch (subsys) {
-          case CDH:
-            FAIL("only the first TM point should be from CDH");
-            break;
-          default:
-            tm_count.at(subsys)++;
-          }
-          AND_THEN("check the increment in TM IDs");
-          REQUIRE(this_tm.id() == (tm_id.at(subsys)++) * 10);
-          AND_THEN("check the data case based on the mock TM ID");
-          REQUIRE(this_tm.data_case() == tm_data_case.at(this_tm.id()));
-          AND_THEN("check that the value is the expected one");
-          switch (this_tm.id()) {
-          case 0:
-            REQUIRE(this_tm.bool_value() == true);
-            break;
-          case 10:
-            REQUIRE(this_tm.double_value() == 3.141592653589793);
-            break;
-          case 20:
-            REQUIRE(this_tm.int_value() == -5000);
-            break;
-          case 30:
-            REQUIRE(this_tm.bytes_value() == subsystem_name(subsys));
-            break;
-          case 40:
-            break; // Nothing to check here, there is no data.
-          case 50:
-            REQUIRE(this_tm.bool_value() == false);
-            break;
-          default:
-            FAIL("unexpected TM_ID: " << this_tm.id());
-          }
-          AND_THEN("repeat");
-          INFO("TM_Frame: " << tm_frame << " SS: " << subsys
-                            << " TM_ID:" << this_tm.id());
-        }
-        REQUIRE(tm_count[PWR] == 6);
-        REQUIRE(tm_count[PWR] == tm_count[IMU]);
-        REQUIRE(tm_count[HMI] == tm_count[IMU]);
+      THEN("if the sequence number is off") {
+        frame.set_timestamp(millis_since_epoch());
+        frame.set_sequence_no(1);
+        cdh::subsystems::process_all_telecommands(frame);
+        INFO("should print a seq_no warning");
+        REQUIRE(true);
       }
     }
   }
